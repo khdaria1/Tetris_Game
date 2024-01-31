360f0e53ad8baf5299595c40e5abe359d631377e
Kyunghyun  Min <khdaria2@gmail.com>
2024-01-30 22:23:14 -0800

    no message

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pygamesys              # Day1
-import sys                    # Day1
+import pygame
+import sys
 #from grid import Grid 
 #from blocks import *
 from game import Game
@@ -16,12 +16,12 @@ game_over_surface = title_font.render("GAME OVER", True, Colors.white)
 score_rect = pygame.Rect(320,55,170,60)
 next_rect = pygame.Rect(320,215,170,180)
 
-screen = pygame.display.set_mode((500,620))      # Day1
-pygame.display.set_caption("Python Tetris")      # Day1
+screen = pygame.display.set_mode((500,620))
+pygame.display.set_caption("Python Tetris")
 
-clock = pygame.time.Clock()                      # Day1
+clock = pygame.time.Clock()
 
-#game_grid = Grid()                              # Day1
+#game_grid = Grid()
 #block = IBlock()
 game = Game()
 
@@ -44,25 +44,30 @@ while True:
                 game.move_right()
             if event.key == pygame.K_DOWN and game.game_over == False:
                 game.move_down()
+                game.update_score(0,1)
             if event.key == pygame.K_UP and game.game_over == False:
                 game.rotate()           
         if event.type == GAME_UPDATE and game.game_over == False:
             game.move_down()
         
     #Drawing
+    score_value_surface = title_font.render(str(game.score), True, Colors.white)
+    
     screen.fill(Colors.dark_blue)      # Day1
     screen.blit(score_surface, (365,20,50,50))
     screen.blit(next_surface, (375,180,50,50))
+    
     if game.game_over == True:
         screen.blit(game_over_surface, (320,450, 50,50))
     
     #game_grid.draw(screen)
     #block.draw(screen)
     pygame.draw.rect(screen, Colors.light_blue, score_rect, 0, 10)
+    screen.blit(score_value_surface,score_value_surface.get_rect(centerx = score_rect.centerx,
+        centery = score_rect.centery))
     pygame.draw.rect(screen, Colors.light_blue, next_rect, 0, 10)
     game.draw(screen)         # Day1
     #game.move_down()
-    
     
     pygame.display.update()   # Day1
     clock.tick(60)            # Day1  game speed
